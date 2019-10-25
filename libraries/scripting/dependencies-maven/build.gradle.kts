11067504d22bb6798cccd38da1fc72d8f0b0fc81
Anatoly Nikitin <Anatoly.Nikitin@jetbrains.com>
2019-10-25 19:26:44 +0300

    removed corountines dependency from kotlin-scripting-dependencies-*

--- libraries/scripting/dependencies-maven/build.gradle.kts
+++ libraries/scripting/dependencies-maven/build.gradle.kts
@@ -11,7 +11,6 @@ dependencies {
     compile(project(":kotlin-scripting-dependencies"))
     compileOnly("org.jetbrains.kotlin:jcabi-aether:1.0-dev-3")
     compileOnly("org.sonatype.aether:aether-api:1.13.1")
-    compile(commonDep("org.jetbrains.kotlinx", "kotlinx-coroutines-core")) { isTransitive = false }
     testCompile(commonDep("junit"))
 }
 
