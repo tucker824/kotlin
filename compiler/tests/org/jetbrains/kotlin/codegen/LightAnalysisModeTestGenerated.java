bd35f355ae562690d18d47dc53965f05c9e097e9
Dmitry Petrov <dmitry.petrov@jetbrains.com>
2019-12-27 16:12:16 +0300

    Fix problem with empty vararg of boxed primitives in JVM_IR
    When calling a generic Java generic method with vararg parameters with empty
    vararg, incorrect array creation instruction was generated for primitive type:
    NEWARRAY T_INT instead of ANEWARRAY java/lang/Integer. Here for Java method
    
      public static <T> void takesVarargOfT(T x1, T... xs) {}
    
    corresponding vararg parameter was considered to be of type 'Array<T>?',
    which is not a non-null array type, so, NewArray intrinsic failed to generate
    proper bytecode.
    
    (cherry picked from commit 98bf0e278f2a7b9d0b2a8ec962493dff8e2893ff)

--- compiler/tests/org/jetbrains/kotlin/codegen/LightAnalysisModeTestGenerated.java
+++ compiler/tests/org/jetbrains/kotlin/codegen/LightAnalysisModeTestGenerated.java
@@ -27196,6 +27196,11 @@ public class LightAnalysisModeTestGenerated extends AbstractLightAnalysisModeTes
             runTest("compiler/testData/codegen/box/vararg/doNotCopyImmediatelyCreatedArrays.kt");
         }
 
+        @TestMetadata("emptyVarargOfBoxedPrimitiveType.kt")
+        public void testEmptyVarargOfBoxedPrimitiveType() throws Exception {
+            runTest("compiler/testData/codegen/box/vararg/emptyVarargOfBoxedPrimitiveType.kt");
+        }
+
         @TestMetadata("kt1978.kt")
         public void testKt1978() throws Exception {
             runTest("compiler/testData/codegen/box/vararg/kt1978.kt");
