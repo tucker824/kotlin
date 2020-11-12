c430ae2069c6599b09a24cb43e0bf724a91025a4
Mads Ager <ager@google.com>
2020-11-12 14:02:57 +0100

    Add build script.

---
+++ google-scripts/build.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+
+import os
+import subprocess
+import sys
+
+BUILD_VERSION = "1.4.20-google-ir-01"
+
+ROOT = os.path.abspath(os.path.normpath(os.path.join(__file__, '..', '..')))
+THIRD_PARTY = os.path.abspath(os.path.normpath(os.path.join(__file__, '..', 'third_party')))
+JDK_DIR = os.path.join(THIRD_PARTY, "jdk8", "linux-x86")
+JDK_9_DIR = os.path.join(THIRD_PARTY, "jdk9", "linux")
+BUNCH = os.path.join(THIRD_PARTY, "bunch-cli-1.1.0", "bin", "bunch")
+
+class ChangedWorkingDirectory(object):
+ def __init__(self, working_directory, quiet=False):
+   self._quiet = quiet
+   self._working_directory = working_directory
+
+ def __enter__(self):
+   self._old_cwd = os.getcwd()
+   if not self._quiet:
+     print 'Enter directory:', self._working_directory
+   os.chdir(self._working_directory)
+
+ def __exit__(self, *_):
+   if not self._quiet:
+     print 'Enter directory:', self._old_cwd
+   os.chdir(self._old_cwd)
+
+def print_cmd(cmd, env=None, quiet=False):
+  if quiet:
+    return
+  if type(cmd) is list:
+    cmd = ' '.join(cmd)
+  if env:
+    env = ' '.join(['{}=\"{}\"'.format(x, y) for x, y in env.iteritems()])
+    print('Running: {} {}'.format(env, cmd))
+  else:
+    print('Running: {}'.format(cmd))
+  sys.stdout.flush()
+
+def get_java_env():
+  java_env = dict(os.environ, JAVA_HOME=JDK_DIR)
+  java_env['PATH'] = java_env['PATH'] + os.pathsep + os.path.join(JDK_DIR, 'bin')
+  java_env['GRADLE_OPTS'] = '-Xmx1g'
+  java_env['JDK_16'] = JDK_DIR
+  java_env['JDK_17'] = JDK_DIR
+  java_env['JDK_18'] = JDK_DIR
+  java_env['JDK_9'] = JDK_9_DIR
+  return java_env
+
+def download_from_google_cloud_storage(sha1_file, bucket='r8-deps', auth=False):
+  download_script = 'download_from_google_storage'
+  cmd = [download_script]
+  if not auth:
+    cmd.append('-n')
+  cmd.extend(['-b', bucket, '-u', '-s',  sha1_file])
+  print_cmd(cmd)
+  subprocess.check_call(cmd)
+
+def download_deps():
+  download_from_google_cloud_storage(os.path.join(THIRD_PARTY, "jdk8", "linux-x86.tar.gz.sha1"))
+  download_from_google_cloud_storage(os.path.join(THIRD_PARTY, "jdk9", "linux.tar.gz.sha1"))
+  download_from_google_cloud_storage(os.path.join(THIRD_PARTY, "bunch-cli-1.1.0.tar.gz.sha1"))
+
+def build_and_upload_compiler():
+  cmd = ["./gradlew", "-Pbuild.number=%s" % BUILD_VERSION, "zipCompiler"]
+  with ChangedWorkingDirectory(ROOT):
+    print_cmd(cmd)
+    subprocess.call(cmd, env=get_java_env())
+  # TODO(ager): Upload to cloud storage.
+  git_reset()
+
+
+def bunch_switch(target):
+  cmd = [BUNCH, "switch", target]
+  with ChangedWorkingDirectory(ROOT):
+    print_cmd(cmd)
+    subprocess.call(cmd)
+
+def git_reset():
+  cmd = ["git", "reset", "--hard"]
+  with ChangedWorkingDirectory(ROOT):
+    print_cmd(cmd)
+    subprocess.call(cmd)
+
+def build_and_upload_android_studio_plugin():
+  bunch_switch("as42")
+  cmd = [
+    "./gradlew",
+    "clean",
+    "cleanupArtifacts",
+    "ideaPlugin",
+    "-Pteamcity=true",
+    "-Pbuild.number=%s" % BUILD_VERSION,
+    "-PdeployVersion=%s" % BUILD_VERSION,
+    "-PpluginVersion=%s-release-Studio4.2-1" % BUILD_VERSION
+  ]
+  with ChangedWorkingDirectory(ROOT):
+    print_cmd(cmd)
+    subprocess.call(cmd, env=get_java_env())
+  cmd = [
+    "./gradlew",
+    "writePluginVersion",
+    "zipPlugin",
+    "-Pteamcity=true",
+    "-Pbuild.number=%s" % BUILD_VERSION,
+    "-PdeployVersion=%s" % BUILD_VERSION,
+    "-PpluginVersion=%s-release-Studio4.2-1" % BUILD_VERSION
+  ]
+  with ChangedWorkingDirectory(ROOT):
+    print_cmd(cmd)
+    subprocess.call(cmd, env=get_java_env())
+  # TODO(ager): Upload to cloud storage.
+
+def main():
+  download_deps()
+  build_and_upload_compiler()
+  build_and_upload_android_studio_plugin()
+
+if __name__ == '__main__':
+  main()
