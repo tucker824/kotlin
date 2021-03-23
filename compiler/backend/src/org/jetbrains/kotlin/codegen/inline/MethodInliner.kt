ebcbf10dda75dc2408419ded5b4742ca007631de
Steven Schäfer <stevenschaefer@google.com>
2021-03-23 15:22:21 +0100

    JVM: Fix usage of ClassBuilder API in AnonymousObjectTransformer

--- compiler/backend/src/org/jetbrains/kotlin/codegen/inline/MethodInliner.kt
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/inline/MethodInliner.kt
@@ -206,6 +206,10 @@ class MethodInliner(
                         ReifiedTypeInliner.putNeedClassReificationMarker(mv)
                         result.reifiedTypeParametersUsages.mergeAll(transformResult.reifiedTypeParametersUsages)
                     }
+
+                    for (classBuilder in childInliningContext.continuationBuilders.values) {
+                        classBuilder.done()
+                    }
                 } else {
                     result.addNotChangedClass(oldClassName)
                 }
