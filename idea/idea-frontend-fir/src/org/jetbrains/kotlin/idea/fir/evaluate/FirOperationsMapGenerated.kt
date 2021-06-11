3803b28a80ee73109a8a31877f3d568cc9f48e1c
Jinseong Jeon <jsjeon@google.com>
2021-06-11 00:42:41 -0700

    Temporarily allow deprecated conversions in FIR operations map
    Also, switch to the new copyright header

--- idea/idea-frontend-fir/src/org/jetbrains/kotlin/idea/fir/evaluate/FirOperationsMapGenerated.kt
+++ idea/idea-frontend-fir/src/org/jetbrains/kotlin/idea/fir/evaluate/FirOperationsMapGenerated.kt
@@ -2,7 +2,7 @@
  * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
-
+@file:Suppress("DEPRECATION", "DEPRECATION_ERROR")
 package org.jetbrains.kotlin.idea.fir.evaluate
 
 import org.jetbrains.kotlin.types.ConstantValueKind
